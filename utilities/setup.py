2538f5e4c43c7a28488e27ea2da05835a6de9d82
user <michael@terminallabs.com>
2017-09-26 11:37:19 -0500

    first commit

---
+++ utilities/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='utilities',
+    version='0.0.1',
+    description='utilities',
+    url='https://github.com/terminal-labs/utilities',
+    author='Michael Verhulst',
+    author_email='michael@terminallabs.com',
+    license="license",
+    packages=['utilities'],
+    zip_safe=False,
+    install_requires=['bash'],
+    )
