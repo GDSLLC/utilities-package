9dcf3761660d1999771d8b70d0797e85e44cf1dc
Michael Verhulst <michael@terminallabs.com>
2018-02-21 11:34:50 -0600

    updated setup.py

--- utilities/setup.py
+++ utilities/setup.py
@@ -2,13 +2,11 @@ import sys
 if sys.version_info.major < 3:
     sys.exit('Python 3 required but lower version found. Aborted.')
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 from setuptools import setup
 
 setup(
     name='utilities-package',
-    version='0.0.3',
+    version='0.0.4',
     description='utilities package',
     url='https://github.com/terminal-labs/utilities',
     author='Terminal Labs',
