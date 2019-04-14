3decdc2afaf6e772b5362e5033d9ecff3bf47a63
verhulstm <michael@terminallabs.com>
2019-04-13 23:37:04 -0500

    added in some tests

--- utilities/test_configs.py
+++
@@ -1,3 +0,0 @@
-from utilities.configs import get_json_dict_from_dir
-
-get_json_dict_from_dir("somefile", "somepath")
---
+++ utilities/tests/test_crypto.py
@@ -0,0 +1,4 @@
+from utilities.crypto import aes_encrypt, aes_decrypt
+
+def test_aes_encrypt():
+    assert aes_decrypt(aes_encrypt('plaintext', 'password'),'password') == b'plaintext' 
