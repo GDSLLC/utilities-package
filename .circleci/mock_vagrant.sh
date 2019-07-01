58540aeffa2c922ba7f418d9a5148496b66fb39d
Michael Verhulst <michael@terminallabs.com>
2019-07-01 14:36:12 -0500

    Create mock_vagrant.sh

---
+++ .circleci/mock_vagrant.sh
@@ -0,0 +1,8 @@
+useradd -m vagrant
+mkdir -p /home/vagrant
+touch /home/vagrant/.bashrc
+
+echo vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
+
+mkdir -p /vagrant
+cp -a /home/circleci/repo/. /vagrant/
