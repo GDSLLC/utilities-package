e25979e891af7fa6968cfa94f608f577c24071c2
Michael Verhulst <michael@terminallabs.com>
2019-07-01 15:12:09 -0500

    Update build_devbox.sh

--- maintenance/build_devbox.sh
+++ maintenance/build_devbox.sh
@@ -2,4 +2,4 @@ vagrant up
 vagrant ssh --command 'cd /vagrant; sudo bash maintenance/install_deps.sh'
 vagrant ssh --command 'cd /vagrant; sudo bash maintenance/install_python.sh'
 vagrant ssh --command 'cd /vagrant; sudo bash maintenance/install_app.sh'
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/test.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash maintenance/run_tests.sh'
