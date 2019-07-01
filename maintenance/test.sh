60adbfbb2a06f928debb52a2f259ae71fd1e68b2
Michael Verhulst <michael@terminallabs.com>
2019-07-01 13:32:44 -0500

    Update test.sh

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -1,2 +1,14 @@
-source env/bin/activate
-pytest
+export PY_ENV_VERSION=3.6.6
+export SERVICE_USER=vagrant
+
+PATH=/home/${SERVICE_USER}/.pyenv/bin:$PATH
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv install -s ${PY_ENV_VERSION}
+pyenv global ${PY_ENV_VERSION}
+pyenv global
+
+utilitiespackage system version
+utilitiespackage system selftest
+utilitiespackage system selfcoverage
