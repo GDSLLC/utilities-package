55d362cd8a1892911ec5e1539726d96a95c085aa
Michael Verhulst <michael@terminallabs.com>
2019-07-01 14:57:10 -0500

    Update test.sh

--- maintenance/test.sh
+++ maintenance/test.sh
@@ -1,12 +1,9 @@
 su -m vagrant <<'EOF'
   export PY_ENV_VERSION=3.6.6
   export SERVICE_USER=vagrant
-  export PY_ENV_VERSION=3.6.6
-  export SERVICE_USER=vagrant
   export PYENV_ROOT="/home/vagrant/.pyenv"
   export PATH="${PYENV_ROOT}/bin:${PATH}"
   export USE_GIT_URI="true"
-
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
   pyenv install -s ${PY_ENV_VERSION}
