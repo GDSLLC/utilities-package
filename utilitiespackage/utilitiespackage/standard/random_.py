a6af7450b050882fe80ffb0e1e96e82eea9fcfee
verhulstm <michael@terminallabs.com>
2019-07-13 20:55:48 -0500

    added tests for random

--- utilitiespackage/utilitiespackage/standard/random_.py
+++ utilitiespackage/utilitiespackage/standard/random_.py
@@ -1,13 +1,14 @@
 import logging
 import random as _random
 
-log = logging.getLogger(__name__)
+def set_random():
+    try:
+        random = _random.SystemRandom()
+    except NotImplementedError: # pragma: no cover
+        log.warn("random.SystemRandom() is not available. Using random.Random() " "instead, this means that things will be less random.")
+        random = _random.Random()
+    globals()["random"] = random
+    return {"random":random}
 
-try:
-    random = _random.SystemRandom()
-except NotImplementedError:
-    log.warn("random.SystemRandom() is not available. Using random.Random() " "instead, this means that things will be less random.")
-    random = _random.Random()
-
-
+set_random()
 __all__ = ["random"]
--- utilitiespackage/utilitiespackage/tests/standard/test_standard_random_.py
+++ utilitiespackage/utilitiespackage/tests/standard/test_standard_random_.py
@@ -1,1 +1,8 @@
+import sys
+del sys.modules['utilitiespackage.standard']
+from utilitiespackage.standard.random_ import set_random
 
+def test_set_random():
+    vars = set_random()
+    assert isinstance(vars, dict)
+    assert len(vars.keys()) == 1
